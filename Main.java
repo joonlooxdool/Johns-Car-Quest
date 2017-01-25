ddd8e898f5f87d3769acce2c4ed8340bdbaa2fea
yoy302 <iomaru777@gmail.com>
2017-01-25 11:33:12 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 package money;
 
 public class Main {
-
+import javax.swing.JFrame;
 	public static void main(String[] args) {
 		// variable in the menu for displaying money will be moneyDisplay
-		
+		JFrame window = mew JFrame();
+		window.setSize(640,480);
 		
 	}
 
