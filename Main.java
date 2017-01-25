200cbfd1d61a14d93fad2f6bede12229968aa4d2
yoy302 <iomaru777@gmail.com>
2017-01-25 11:38:55 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,9 @@ import javax.swing.JFrame;
 		// variable in the menu for displaying money will be moneyDisplay
 		JFrame window = mew JFrame();
 		window.setSize(640,480);
-		
+		window.setTitle("Please help me I'm autistic :3");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 	}
 
 }
