92f10013644fa7fa9dc6dabf43f095807559eb80
dapeep <dapeep24@gmail.com>
2017-02-16 12:59:07 -0600

    Update Display.java

--- Display.java
+++ Display.java
@@ -13,34 +13,9 @@ public class Display {
 	public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
 		
 		Graphics gui = new Graphics();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setVisible(true);
-		gui.setTitle("biggiebheese");
-		gui.imgLoader();
-		gui.pack();
-		gui.setSize(1920, 1080);
+		gui.draw();
 		
-		//Paint G = new Paint();
-		//gui.add(G);
-		//G.paintComponent(null);
-		
-		//Music M = new Music();
-		//M.playMusic();
-		
-		//ImagePanel panel = new ImagePanel();
-		//new ImageIcon("jon sprite.png").getImage();
-
-		//JFrame frame = new JFrame("jon tron");
-		//gui.getContentPane().add(panel);
-		//frame.pack();
-		//frame.setVisible(true);
-
-		
-		gui.setVisible(true);
-		//gui.ImagePanel("jon sprite.png");
-				
-				
-		new Sprites();
+		//new Sprites();
 		
 		
 		
